90ed1fc45e8adc0ac111e60e0b33202e2ee7f983
William Woods <54908283+Will-bit@users.noreply.github.com>
2019-11-12 10:53:51 -0600

    Create app.js

---
+++ app.js
@@ -0,0 +1,95 @@
+
+// 7..
+function sayHello() {
+    alert("Hello World!");
+};
+
+sayHello();
+///3
+let will = "William";
+console.log(will)
+//4
+const states = 50;
+console.log(states)
+//5
+let isWill = 5 + 4
+console.log(isWill)
+/// Using different Arrayconst veg = new Array(Broccoli, Asparagus, Spinich, Celery, Tomatoes, Cabbage);
+/// 9.
+var veg = ["Broccoli", "Celery", "Carrots", "Tomatoes", "Spinich"];
+veg.forEach(function (entry) {
+    console.log(entry);
+});
+
+//6
+
+//8.
+function checkAge = oldAge(name, age)
+limit = 21
+if  age <= limit
+    checkAge = true;
+elseif age >= limit
+checkAge = false;
+else
+console.log('Sorry' + "you're not old enough!")
+
+const name = ['Will', 'Josh', 'Ryan', 'Scott', 'Meg'];
+const age = ['19', '24', '16', '22', '23']
+
+const person = {
+    firstName: ['Charles', 'Abby', 'James', "John", "Meg"]
+    age: [23, 27, 18, 17, 22]
+}
+
+console.log(person.firstName, person.lastName)
+function sayHello("")
+
+
+//12
+
+function getLength(2) {
+    for (let i = 1; i < 2; i++) {
+        if (i % 2 !== 0)
+            console.log("Odd Number")
+        else if
+            (i % 2 === 0) {
+            console.log("Even Number");
+
+
+
+            // For
+            for (let i = 0; i < 10; i++) {
+                console.log();
+
+            }
+
+            //While
+
+            let i = 0;
+            while (i < 10) {
+                console.log("test");
+                i++;
+            }
+
+
+            // for (let todo of todos){
+            //    console.log
+
+
+            if (x < 21) {
+                console.log("Sorry" + name + "you're not old enough to view this webpage!")
+            } else {
+                console.log(name "your safe!")
+            }
+
+
+
+            function checkAge(num1, num2) {
+                console.log()
+            }
+
+
+
+            console.log(veg[]);
+
+            console.log(s.length)
